e69b5ab95a606c38a4aa037a23d29c2788acc72a
deborahly <97473884+deborahly@users.noreply.github.com>
2022-06-18 17:58:45 -0400

    Register models to admin site

--- anki/admin.py
+++ anki/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import BasicCard, CardDeck, User
+
+class CardDeckAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user')
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(CardDeck, CardDeckAdmin)
+admin.site.register(BasicCard)
