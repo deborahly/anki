9958349abf0a84f6063fbe503491ded5fa9a0611
deborahly <97473884+deborahly@users.noreply.github.com>
2022-06-18 18:00:15 -0400

    Add url for save view

--- anki/urls.py
+++ anki/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('save/<str:type>', views.save, name='save'),
     path('login', views.login_view, name='login'),
     path('register', views.register_view, name='register'),
     path('logout', views.logout_view, name='logout'),
