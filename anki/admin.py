20686e7e3252b4917842e3160bd6187c8cffd207
deborahly <97473884+deborahly@users.noreply.github.com>
2022-06-21 01:27:08 -0400

    Add configuration to admin site

--- anki/admin.py
+++ anki/admin.py
@@ -4,7 +4,10 @@ from .models import BasicCard, CardDeck, User
 class CardDeckAdmin(admin.ModelAdmin):
     list_display = ('name', 'user')
 
+class BasicCardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'grammar_class', 'easiness', 'front', 'front_extra', 'back_main', 'back_alt_1', 'back_alt_2', 'deck')
+
 # Register your models here.
 admin.site.register(User)
 admin.site.register(CardDeck, CardDeckAdmin)
-admin.site.register(BasicCard)
+admin.site.register(BasicCard, BasicCardAdmin)
