e094d1243b0c82535241fee1d70360cb2c4d3618
deborahly <97473884+deborahly@users.noreply.github.com>
2022-06-28 14:58:30 -0400

    Deleted code for unarchive feature

--- anki/static/anki/archive.js
+++ anki/static/anki/archive.js
@@ -1,33 +1,3 @@
-// Unarchive when button is clicked:
-document.querySelectorAll('.unarchive-btn').forEach(element => {
-    element.addEventListener('click', () => {                
-        const csrftoken = getCookie('csrftoken');
-        
-        const deck_id = element.dataset.id;
-        
-        const unarchiveDeck = async (deck_id) => {
-            try {
-                const response = await fetch('http://127.0.0.1:8000/unarchive', {
-                    method: 'POST',
-                    headers: {
-                        'X-CSRFToken': csrftoken,
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({deck_id: deck_id})
-                })
-                if (response.ok) {
-                    const list_element = document.getElementById(`li-${deck_id}`);
-                    list_element.remove();
-                }
-            } catch (error) {
-                console.error(error);
-            } 
-        }
-        unarchiveDeck(deck_id);
-    })
-});
-
-// Delete when button is clicked:
 document.querySelectorAll('.delete-deck-btn').forEach(element => {
     element.addEventListener('click', () => {                
         var result = confirm('Are you sure to delete?');
@@ -47,8 +17,8 @@ document.querySelectorAll('.delete-deck-btn').forEach(element => {
                         body: JSON.stringify({deck_id: deck_id})
                     })
                     if (response.ok) {
-                        const list_element = document.getElementById(`li-${deck_id}`);
-                        list_element.remove();
+                        document.getElementById(`li-${deck_id}`).remove();
+                        document.getElementById('message').innerHTML = 'Deck deleted.'
                     }
                 } catch (error) {
                     console.error(error);
